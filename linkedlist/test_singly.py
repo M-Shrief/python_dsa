8e45e5f36e71fe3393eb668a2cf51faa4826e027
m-shrief <m.shrief.dev@proton.me>
2024-12-19 12:03:07 +0200

    Updated with better structure and used comments to add hints if the test failed...etc.

--- linkedlist/test_singly.py
+++ linkedlist/test_singly.py
@@ -5,7 +5,12 @@ def test_singly():
     singlyList.add_last(1)
     singlyList.add_last(2)
     singlyList.add_last(3)
-    
-    assert singlyList.get_head().data == 1
+
+    assert singlyList.get_size() == 3, "Size should be 3"
+
+    head = singlyList.get_head()
+    assert head.data == 1
+    assert head.next.data == 2
+    assert head.next.next.data == 3
 
 
