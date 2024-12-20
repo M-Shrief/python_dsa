014cc2e1734ceca3e10fded90dd11210e5a82040
m-shrief <m.shrief.dev@proton.me>
2024-12-20 16:53:38 +0200

    Added Binary Search Tree, with insert() method, and its tests.

---
+++ trees/bst.py
@@ -0,0 +1,46 @@
+from typing import TypeVar, Generic
+
+T = TypeVar("T")
+
+class BSNode(Generic[T]):
+    """Binary Search Tree node"""
+    def __init__(self, val: T):
+        self.val = val
+        self.left: BSNode | None = None
+        self.right: BSNode | None = None
+
+class BST(Generic[T]):
+    """Binary Search Tree"""
+    def __init__(self):
+        self.__root: BSNode | None = None
+        self.__size: int = 0
+    
+    def get_size(self)->int:
+        return self.__size
+    
+    def get_root(self)-> BSNode | None:
+        return self.__root
+    
+    def insert(self, val: T):
+        if(self.__size == 0):
+            newNode = BSNode[T](val)
+            self.__root = newNode
+            self.__size += 1
+        else:
+            self.__insert_node(self.__root, val)
+    
+    def __insert_node(self, node: BSNode, val: T):
+        if(val <= node.val):
+            if(node.left is not None):
+                self.__insert_node(node.left, val)
+            else:
+                newNode = BSNode[T](val)
+                node.left = newNode
+                self.__size += 1
+        else:
+            if(node.right is not None):
+                self.__insert_node(node.right, val)
+            else:
+                newNode = BSNode[T](val)
+                node.right = newNode
+                self.__size += 1
---
+++ trees/test_bst.py
@@ -0,0 +1,33 @@
+from .bst import BST
+from ..utils import errors
+
+class TestBST:
+
+    def test_insert(self):
+        bst = BST[int]()
+        # if we inserted 5,2,1,3,7,6,8
+        # bst should Look like this:
+        #            5
+        #        2		7
+        #    1	  3	  6    8
+        bst.insert(5)
+        bst.insert(2)
+        bst.insert(1)
+        bst.insert(3)
+        bst.insert(7)
+        bst.insert(6)
+        bst.insert(8)
+
+        assert bst.get_size() == 7, "Size should be 7"
+
+        root = bst.get_root()
+        assert root.val == 5, errors.wrong_value(root.val, 5)
+
+        assert root.left.val == 2, errors.wrong_value(root.left.val, 2)
+        assert root.right.val == 7, errors.wrong_value(root.right.val, 7)
+
+        assert root.left.left.val == 1, errors.wrong_value(root.left.left.val, 1)
+        assert root.left.right.val == 3, errors.wrong_value(root.left.right.val, 3)
+
+        assert root.right.left.val == 6, errors.wrong_value(root.right.left.val, 6)
+        assert root.right.right.val == 8, errors.wrong_value(root.right.right.val, 8)
