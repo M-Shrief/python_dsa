47a2a44689b049f67119fb203a989aec0fcee40e
m-shrief <m.shrief.dev@proton.me>
2025-08-12 19:38:50 +0300

    Updated wrong_value() parameters to use Any type.

--- utils/errors.py
+++ utils/errors.py
@@ -1,5 +1,5 @@
-
-def wrong_value(got: int, want: int)->str:
+from typing import Any
+def wrong_value(got: Any, want: Any)->str:
     """Returns simple message, showing the value we got and the value we want.
     Note: it may become more verbose and not clear if used while coding, sometimes it's better to write the error string directly."""
     return f"Got: {got}, Want: {want}"
